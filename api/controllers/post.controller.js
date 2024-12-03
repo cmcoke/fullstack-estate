38a4ed936a0d855c7d2a1ab6d769602efb0d6cab
cmcoke <minoru.cm@gmail.com>
2024-12-03 13:39:47 -0500

    allowed logged in user to create and save posts and also view the posts on their profile.

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -3,11 +3,26 @@ import jwt from "jsonwebtoken";
 
 export const getPosts = async (req, res) => {
 
+  const query = req.query;
+
   try {
 
-    const posts = await prisma.post.findMany();
+    const posts = await prisma.post.findMany({
+      where: {
+        city: query.city || undefined,
+        type: query.type || undefined,
+        property: query.property || undefined,
+        bedroom: parseInt(query.bedroom) || undefined,
+        price: {
+          gte: parseInt(query.minPrice) || undefined,
+          lte: parseInt(query.maxPrice) || undefined,
+        },
+      },
+    });
+
 
     res.status(200).json(posts);
+
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to get posts" });
@@ -35,7 +50,49 @@ export const getPost = async (req, res) => {
       },
     });
 
-    res.status(200).json(post);
+    // const token = req.cookies?.token;
+
+    // if (token) {
+    //   jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+    //     if (!err) {
+    //       const saved = await prisma.savedPost.findUnique({
+    //         where: {
+    //           userId_postId: {
+    //             postId: id,
+    //             userId: payload.id,
+    //           },
+    //         },
+    //       });
+    //       res.status(200).json({ ...post, isSaved: saved ? true : false });
+    //     }
+    //   });
+    // }
+
+    let userId;
+    const token = req.cookies?.token;
+
+    if (!token) {
+      userId = null;
+    } else {
+      jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+        if (err) {
+          userId = null;
+        } else {
+          userId = payload.id;
+        }
+      });
+    }
+
+    const saved = await prisma.savedPost.findUnique({
+      where: {
+        userId_postId: {
+          postId: id,
+          userId
+        }
+      }
+    });
+
+    res.status(200).json({ ...post, isSaved: saved ? true : false });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to get post" });
